b6052da8ea0cafa3d6f8412288ed8e0481db883c
johnolek <johnoleksowicz@icloud.com>
2026-03-11 21:54:06 -0500

    Remove unnecessary presets

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,10 +2,7 @@ export const LETTERS = 'abcdefghijklmnopqrstuvwxyz'.split('');
 export const EXTRAS  = '.,;:!?\'-/"@'.split('');
 
 export const PRESETS = {
-  'me-taps':      'aehinorst',
-  'me-taps+dl':   'adehinlorst',
-  'me-taps+dlcu': 'acdehinlorstu',
-  'me-common':    'acdefghilmnorstwu',
-  'all':          LETTERS.join(''),
-  'none':         '',
+  'MessagEase Taps': 'aehinorst',
+  'all':             LETTERS.join(''),
+  'none':            '',
 };
